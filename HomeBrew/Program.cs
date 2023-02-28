2dcc48df03563c06287f42fd78de8bc65f343361
Jody VanHoose <jodyvanhoose80@gmail.com>
2023-02-28 17:40:39 -0500

    Added TitleScreen class

--- HomeBrew/Program.cs
+++ HomeBrew/Program.cs
@@ -7,43 +7,49 @@ namespace HomeBrew
         static void Main(string[] args)
         {
             // Program start
-            Console.WriteLine("Welcome to the Pawtucket Homebrew Tool!");
-            Console.WriteLine("Would you like to start building a recipe or use a homebrew tool?");
-            Console.WriteLine("--------------------------");
-            Console.WriteLine("Buid a Recipe: 1");
-            Console.WriteLine("Homebrew Tools:");
-            Console.WriteLine("Brix to Specfic Gravity Converter: 2");
-            Console.WriteLine("ABV Calculator: 3");
-            Console.WriteLine("Exit: 0");
-            Console.Write("Your input: ");
+
+            TitleScreen.Title();
+            TitleScreen.TitleMenu();
+
+            // Variables for selecting menu options
             string UserInput = Console.ReadLine();
+            string RecipeSelect = "1";
+            string BrixSelect = "2";
+            string AbvSelect = "3";
+            string ExitProgram = "0";
             
 
             // Master Loop While not exit execute this loop
-            while(UserInput != "0")
+            while(UserInput != ExitProgram)
             {
                 // Recipe 
-                if (UserInput == "1")
+                if (UserInput == RecipeSelect)
                 {
                     Console.Clear();
-                    Console.WriteLine("Let's build a recipe.");
+                    TitleScreen.Title();
+                    Console.WriteLine("Recipe Builder");
+                    Console.WriteLine("--------------");
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
                 }
                 // Homebrew tools
-                else if (UserInput == "2")
+                else if (UserInput == BrixSelect)
                 {
                     Console.Clear();
+                    TitleScreen.Title();
                     Console.WriteLine("Welcome to the homebrew tools.");
                     Console.WriteLine("Brix to Specfic Gravity Converter");
+                    Console.WriteLine("---------------------------------");
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
                 }
-                else if (UserInput == "3")
+                else if (UserInput == AbvSelect)
                 {
                     Console.Clear();
+                    TitleScreen.Title();
                     Console.WriteLine("Welcome to the homebew tools.");
                     Console.WriteLine("ABV Calculator");
+                    Console.WriteLine("--------------");
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
                 }
@@ -52,6 +58,7 @@ namespace HomeBrew
                 else
                 {
                     Console.Clear();
+                    TitleScreen.Title();
                     Console.WriteLine("Sorry invalid input");
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
@@ -59,14 +66,16 @@ namespace HomeBrew
                     
                 }
                 Console.Clear();
-                Console.WriteLine("Would you like to start building a recipe or use a homebrew tool?");
-                Console.WriteLine("--------------------------");
-                Console.WriteLine("Buid a Recipe: 1");
-                Console.WriteLine("Homebrew Tools:");
-                Console.WriteLine("Brix to Specfic Gravity Converter: 2");
-                Console.WriteLine("ABV Calculator: 3");
-                Console.WriteLine("Exit: 0");
-                Console.Write("Your input: ");
+                TitleScreen.Title();
+                TitleScreen.TitleMenu();
+                //Console.WriteLine("Would you like to start building a recipe or use a homebrew tool?");
+                //Console.WriteLine("--------------------------");
+                //Console.WriteLine("Buid a Recipe: 1");
+                //Console.WriteLine("Homebrew Tools:");
+                //Console.WriteLine("Brix to Specfic Gravity Converter: 2");
+                //Console.WriteLine("ABV Calculator: 3");
+                //Console.WriteLine("Exit: 0");
+                //Console.Write("Your input: ");
                 UserInput = Console.ReadLine();
             }
 
@@ -74,6 +83,7 @@ namespace HomeBrew
 
             // Program End
             Console.Clear();
+            TitleScreen.Title();
             Console.WriteLine("Thanks for using the Homebrew Tool!");
             Console.WriteLine("Exiting Program");
             Environment.Exit(0);
---
+++ HomeBrew/TitleScreen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBrew
+{
+    public static class TitleScreen
+    {
+        // Displays Title
+        public static void Title()
+        {
+            Console.WriteLine("   *****                                                                                     ");
+            Console.WriteLine("  *******           *   *                          *                                *        ");
+            Console.WriteLine("  *      ***        *   *                          *                                *          ");
+            Console.WriteLine("  *      *  *       *   *  * * *  *     *   * *    *       * *   * *                *      ");
+            Console.WriteLine("  *      *  *       * * *  *   *  * * * *  *    *  * *    *     *    *  *        *  *         ");
+            Console.WriteLine("  *      *  *       *   *  *   *  *  *  *  * * *   *   *  *     * * *    *      *   *        ");
+            Console.WriteLine("  *      ***        *   *  *   *  *     *  *       *   *  *     *         * ** *              ");
+            Console.WriteLine("  ********          *   *  * * *  *     *   * *    * * *  *      * *       *  *     *       ");
+
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+        // Displays Menu
+        public static void TitleMenu()
+        {
+            Console.WriteLine("Welcome to the Pawtucket Homebrew Tool!");
+            Console.WriteLine("Would you like to start building a recipe or use a homebrew tool?");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Menu options");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Buid a Recipe:     Press 1");
+            Console.WriteLine("--------------------------");
+            // Console.WriteLine("Homebrew Tools:");
+            Console.WriteLine("Brix to Specfic");
+            Console.WriteLine("Gravity Converter: Press 2");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("ABV Calculator:    Press 3");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Exit:              Press 0");
+            Console.WriteLine("--------------------------");
+            Console.Write("Your input: ");
+        }
+    }
+}
