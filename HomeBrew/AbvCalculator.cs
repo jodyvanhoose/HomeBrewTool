5c8f79134f4354a3634f4cc06d87bd6c89987f4f
Jody VanHoose <jodyvanhoose80@gmail.com>
2023-02-27 22:05:01 -0500

    added ABVCalculator class and the master loop structure for main program

---
+++ HomeBrew/AbvCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBrew
+{
+    public class AbvCalculator
+    {
+        public double CalculateABV(double og, double fg)
+        {
+            // Calculates the ABV from the original gravity and final gravity
+            double ABV = ABV = (og - fg) * 131.25;
+            return ABV;
+        }
+    }
+}
--- HomeBrew/Program.cs
+++ HomeBrew/Program.cs
@@ -11,13 +11,15 @@ namespace HomeBrew
             Console.WriteLine("Would you like to start building a recipe or use a homebrew tool?");
             Console.WriteLine("--------------------------");
             Console.WriteLine("Buid a Recipe: 1");
-            Console.WriteLine("Homebrew Tools: 2");
+            Console.WriteLine("Homebrew Tools:");
+            Console.WriteLine("Brix to Specfic Gravity Converter: 2");
+            Console.WriteLine("ABV Calculator: 3");
             Console.WriteLine("Exit: 0");
             Console.Write("Your input: ");
             string UserInput = Console.ReadLine();
             
 
-            // While not exit execute this loop
+            // Master Loop While not exit execute this loop
             while(UserInput != "0")
             {
                 // Recipe 
@@ -25,27 +27,50 @@ namespace HomeBrew
                 {
                     Console.Clear();
                     Console.WriteLine("Let's build a recipe.");
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
                 }
                 // Homebrew tools
                 else if (UserInput == "2")
                 {
                     Console.Clear();
                     Console.WriteLine("Welcome to the homebrew tools.");
-                    Console.WriteLine("Which tool do you want to use?");
-                    Console.WriteLine("Brix to Specfic Gravity Converter: 1");
-                    Console.WriteLine("ABV Calculator: 2");
-                    Console.WriteLine("Exit: 0");
-                    Console.Write("Your input: ");
-                    UserInput = Console.ReadLine();
-                    Console.WriteLine(UserInput);
+                    Console.WriteLine("Brix to Specfic Gravity Converter");
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
                 }
+                else if (UserInput == "3")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the homebew tools.");
+                    Console.WriteLine("ABV Calculator");
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
+                }
+
                 // Invalid input
                 else
                 {
+                    Console.Clear();
                     Console.WriteLine("Sorry invalid input");
-                    UserInput = Console.ReadLine();
+                    Console.WriteLine("Press enter to continue");
+                    Console.ReadLine();
+                    
+                    
                 }
+                Console.Clear();
+                Console.WriteLine("Would you like to start building a recipe or use a homebrew tool?");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Buid a Recipe: 1");
+                Console.WriteLine("Homebrew Tools:");
+                Console.WriteLine("Brix to Specfic Gravity Converter: 2");
+                Console.WriteLine("ABV Calculator: 3");
+                Console.WriteLine("Exit: 0");
+                Console.Write("Your input: ");
+                UserInput = Console.ReadLine();
             }
+
+
 
             // Program End
             Console.Clear();
